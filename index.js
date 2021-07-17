ae076d52a930852dc7cf893b0feaa0d9d69f778b
Tanner <tanne@DESKTOP-0CHF72S.Home>
2021-07-17 15:39:39 -0700

    Style consistancy

--- index.js
+++ index.js
@@ -6,26 +6,28 @@ dotenv.config();
 const client = new Discord.Client();
 const apiKey = process.env.DISCORD_API_KEY;
 const prefix = '!';
+const superUser = '866067521118535730'
 const commandFile = fs.readdirSync('./commands/').filter(file => file.endsWith(".js"));
 
 client.commands = new Discord.Collection();
 commandFile.forEach(element => {
     const command = require(`./commands/${element}`);
     client.commands.set(command.name, command);
-})
+});
 
 client.once('ready', ()=>{
     console.log("Shell ready");
-})
+});
 
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot){return}
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
-    if(!client.commands.get(cmd.toString())){return}
-    client.commands.get(cmd.toString()).execute(message, args);
 
-})
+    if(!client.commands.get(cmd.toString())){return}
+
+    client.commands.get(cmd.toString()).execute(message, args, superUser);
+});
 
 client.login(apiKey).catch(error =>{
     console.log("Wrong key.");
