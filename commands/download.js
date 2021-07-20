0f91423f75e582f24805bbe7e1b3e54398936fae
Tanner <tanne@DESKTOP-0CHF72S.Home>
2021-07-19 21:49:17 -0700

    Added comments

--- commands/download.js
+++ commands/download.js
@@ -2,12 +2,16 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 module.exports = {
     name: 'down',
-    description: 'Downloads a file from the message to the server. Takes the file name as argument.',
+    description: 'Downloads a file from the message to the server. Takes the file name as argument. Creates a directory for the channel that the command was called from',
     execute(message, args, role){
         const channelName = message.channel.name;
-        fs.mkdirSync(channelName, { recursive: true }, (err) => {if (err) console.log(err);})
+        //Make a directory for the channel that the command was called on.
+        fs.mkdirSync(channelName, { recursive: true }, (err) => {if (err) console.log(err);});
+        //Create a fetch request from the url that is associated with the messages attachment.
         fetch(message.attachments.first().url).then(res => {
-            const dest = fs.createWriteStream("./"+channelName+"/"+args);
+            //Create a write stream for the file we are going to create
+            const dest = fs.createWriteStream(`./${channelName}/${args}`);
+            //pipe the file to the destination.
             res.body.pipe(dest);
         }).catch(error => console.log(error));
     }
--- commands/upload.js
+++ commands/upload.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'up',
     description: 'Uploads a file from the bot\'s server to the called channel.',
     execute(message, args, role){
+        //Create an attachment and send it to the channel that the command was sent from
         const attachment = new MessageAttachment('./'+args);
         message.channel.send(attachment);
     }
