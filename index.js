74815e7f1781e3c890c4ce8f9e5659237d307d9c
Tanner <tanne@DESKTOP-0CHF72S.Home>
2021-07-19 22:05:11 -0700

    Added comments

--- index.js
+++ index.js
@@ -3,22 +3,28 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const client = new Discord.Client();
-const apiKey = process.env.DISCORD_API_KEY;
-const prefix = '!';
-const superUser = '866067521118535730'
-const commandFile = fs.readdirSync('./commands/').filter(file => file.endsWith(".js"));
+const client = new Discord.Client(); //Create session does not start the session
+const apiKey = process.env.DISCORD_API_KEY;// Grab the API key from the .env file
+const prefix = '!'; //the prefix is what we use to tell the bot that we're issuing a command
+const superUser = process.env.SUPER_USER_ID;// Grab the super user id from the .env file
+const commandFile = fs.readdirSync('./commands/').filter(file => file.endsWith(".js"));//grab all command files from the commands folder
 
+//Adds every command to a discord collection 
 client.commands = new Discord.Collection();
 commandFile.forEach(element => {
     const command = require(`./commands/${element}`);
     client.commands.set(command.name, command);
 });
 
+//Lets the user know that the bot is running
 client.once('ready', ()=>{
     console.log("Shell ready");
 });
-
+/*
+ * On each message the bot will check the prefix previously established, and make sure that the author wasn't the bot itself.
+ * It uses a regex expression to split the command on any white space
+ * We pop off the first arguement which is the command for the bot, then check that the command exsists, if it does it gets executed.
+ */
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot){return}
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -29,6 +35,7 @@ client.on('message', message =>{
     client.commands.get(cmd.toString()).execute(message, args, superUser);
 });
 
+//Logs the bot in with the appropriate API key
 client.login(apiKey).catch(error =>{
     console.log("Wrong key.");
-}); //API key here*/
+});
