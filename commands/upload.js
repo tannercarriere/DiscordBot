571c4a268e4c922726fe0c45a67e5f780515c7d7
Tanner <tanne@DESKTOP-0CHF72S.Home>
2021-07-25 18:01:24 -0700

    Now catches FileNotFoundErrors

--- commands/upload.js
+++ commands/upload.js
@@ -2,9 +2,13 @@ const { MessageAttachment } = require('discord.js');
 module.exports = {
     name: 'up',
     description: 'Uploads a file from the bot\'s server to the called channel.',
-    execute(message, args, role){
+    execute(message, args, dir){
+        curDirectory = "";
+        dir.forEach(value => {
+            curDirectory += `${value}/`;
+        });
         //Create an attachment and send it to the channel that the command was sent from
-        const attachment = new MessageAttachment('./'+args);
-        message.channel.send(attachment);
+        const attachment = new MessageAttachment(curDirectory+args);
+        message.channel.send(attachment).catch(err =>{message.channel.send("File not found.")});
     }
 }
