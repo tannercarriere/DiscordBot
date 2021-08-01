fee65622792fbab09dc7cfb1146adc5f7461e856
Tanner <tanne@DESKTOP-0CHF72S.Home>
2021-08-01 16:15:13 -0700

    Added checks to make sure message is valid
    Added check to make sure that the file has a name and attachment.

--- commands/download.js
+++ commands/download.js
@@ -4,11 +4,20 @@ module.exports = {
     name: 'down',
     description: 'Downloads a file from the message to the server. Takes the file name as argument. Creates a directory for the channel that the command was called from',
     execute(message, args, role){
+        let file = message.attachments.first()
+        if(args.length === 0){
+            message.channel.send("You need to specify a file name.");
+            return;
+        }
         const channelName = message.channel.name;
         //Make a directory for the channel that the command was called on.
         fs.mkdirSync(channelName, { recursive: true }, (err) => {if (err) console.log(err);});
         //Create a fetch request from the url that is associated with the messages attachment.
-        fetch(message.attachments.first().url).then(res => {
+        if(!file){
+            message.channel.send("You need to attach a file to download.");
+            return;
+        }
+        fetch(file.url).then(res => {
             //Create a write stream for the file we are going to create
             const dest = fs.createWriteStream(`./${channelName}/${args}`);
             //pipe the file to the destination.
